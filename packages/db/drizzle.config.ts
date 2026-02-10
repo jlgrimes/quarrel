b5fdc9379b938977102121bb223e5ceb698547e8
Jared Grimes <jaredlancegrimes@gmail.com>
2026-02-10 15:09:57 -0600

    updated dialect to exist

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: "sqlite",
-  driver: "turso",
+  dialect: "turso",
   schema: "./schema/index.ts",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
